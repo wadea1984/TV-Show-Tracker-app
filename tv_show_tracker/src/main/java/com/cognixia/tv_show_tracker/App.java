322c58b0bb80c22a4e2df881de4572ca1f34da24
wadea1984 <wadea1984@gmail.com>
2025-06-24 12:01:13 -0400

    created maven project

---
+++ tv_show_tracker/src/main/java/com/cognixia/tv_show_tracker/App.java
@@ -0,0 +1,13 @@
+package com.cognixia.tv_show_tracker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
